bf1659d374e735bca16a5754ca16246b31776899
Ubuntu <ubuntu@ip-10-248-213-124.eu-west-1.compute.internal>
2026-03-26 09:57:43 +0000

    fix - added toy data processing example to be accelerated with OpenRNG

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,10 @@
 
 using namespace std;
 
-void generateDistribution(const int NUM_ELEMENTS, BASIC_RNG distrubutionA, BASIC_RNG distrubutionB, const std::pair<float, float>& argA, const std::pair<float, float>& argB){
+Vec1D generateDistribution(const int NUM_ELEMENTS, BASIC_RNG distrubutionA, BASIC_RNG distrubutionB, const std::pair<float, float>& argA, const std::pair<float, float>& argB){
     
-    auto title = std::string{"with APL Generating Distribution of size "} + std::to_string(NUM_ELEMENTS);
-    Timer A(title.c_str(), UNIT::US);
+    // auto title = std::string{"Generating Distribution of size "} + std::to_string(NUM_ELEMENTS);
+    // Timer A(title.c_str(), UNIT::US);
     
     float paramA = argA.first;
     float paramB = argA.second;
@@ -26,6 +26,7 @@ void generateDistribution(const int NUM_ELEMENTS, BASIC_RNG distrubutionA, BASIC
     if (sum.has_value()){
         // print out the sum of the 2 distributions.
         auto& result = sum.value();
+        return result;
     } else{
         std::cout << "sizes of vec1D objects are not the same " << std::endl;
     }
@@ -36,12 +37,30 @@ void generateDistribution(const int NUM_ELEMENTS, BASIC_RNG distrubutionA, BASIC
 
 int main(){
     
+    const int NUM_POINTS = (1<<14); // 2^14 (16,384) random points
+
     std::pair<float, float> meanAndStdDeviationParams{30.0,50.0}; // Key parameters for a Gaussian Distribution
     std::pair<float, float> minAndMaxParams{10.0,100.0}; // Key parameters for a uniform distribution
 
-    generateDistribution((1<<14), BASIC_RNG::GAUSSIAN, BASIC_RNG::UNIFORM, meanAndStdDeviationParams, minAndMaxParams);
+    // STEP 1. Generate a distribution of 2D Points that is the sum of a Gaussian and Uniform distribution
 
+    const Vec1D distribution  = generateDistribution(NUM_POINTS, BASIC_RNG::GAUSSIAN, BASIC_RNG::UNIFORM, meanAndStdDeviationParams, minAndMaxParams);
 
+    // STEP 2. Calculate the number of points that fit within a 2D window
+
+    Rectangle window(10.0,10.0,50.0,50.0);
+    int numberOfPoints = window.countPointsInRectangle(distribution);
+    std::cout << "Number of Data Points = " << NUM_POINTS 
+              << " | Number of Points within Window ( [" 
+              << window.bottomLeft[0]  << ", " << window.bottomLeft[1] << "] , ["
+              << window.topRight[0]  << ", " << window.topRight[1]
+              << "] ) = " << numberOfPoints << std::endl;
+    
+    
+    // STEP 3. Calculate the magnitude of the smallest point within the distribution
+
+    float shortestDistance = min_length(distribution.getData());
+    std::cout << "Shortest Distance from Origin = " << shortestDistance << std::endl;
 
     return 0;
 }
